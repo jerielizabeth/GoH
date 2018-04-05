3468c1b974c141833bd7fa0e669cd0de2cacdea8
Jeri E. Wieringa <jeri.elizabeth@gmail.com>
2018-04-05 11:11:44 -0700

    added requirements file

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-# import sphinx_bootstrap_theme
+import sphinx_bootstrap_theme
 import sys
 sys.path.insert(0, os.path.abspath('../GoH/'))
 
@@ -104,12 +104,12 @@ import sphinx_rtd_theme
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 # html_theme = "sphinx_rtd_theme"
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# html_theme = 'bootstrap'
-# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
